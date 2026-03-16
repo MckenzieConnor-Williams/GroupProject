a7b649741af75ae52d4a51650c9f810ccc33bb97
3689999 <ARSH.DUGGAL@stu.mmu.ac.uk>
2026-03-16 11:58:19 +0000

    mkl

--- homepage.js
+++ homepage.js
@@ -6,26 +6,57 @@ let topicsDiv = document.getElementById("dropdown-div-topics");
 
 let signupBtn = document.getElementById("signup-btn");
 
-signupBtn.onclick = () => {
-    window.location.href = "signup.html";
-}
+(function () {
+    const cards = document.querySelectorAll(".option-card[data-route]");
 
-studyTool.onclick = () => {
-    if(studyToolDiv.style.display == "block"){
-        studyToolDiv.style.display = "none";
+    if (signupBtn) {
+        signupBtn.onclick = () => {
+            window.location.href = "signup.html";
+        };
     }
-    else{
-        studyToolDiv.style.display = "block";
-        topicsDiv.style.display = "none";
-    }
-}
 
-topics.onclick = () => {
-    if(topicsDiv.style.display == "block"){
-        topicsDiv.style.display = "none";
+    if (studyTool) {
+        studyTool.onclick = () => {
+            if (studyToolDiv && studyToolDiv.style.display === "block") {
+                studyToolDiv.style.display = "none";
+            } else if (studyToolDiv) {
+                studyToolDiv.style.display = "block";
+                if (topicsDiv) {
+                    topicsDiv.style.display = "none";
+                }
+            }
+        };
     }
-    else{
-        topicsDiv.style.display = "block";
-        studyToolDiv.style.display = "none";
+
+    if (topics) {
+        topics.onclick = () => {
+            if (topicsDiv && topicsDiv.style.display === "block") {
+                topicsDiv.style.display = "none";
+            } else if (topicsDiv) {
+                topicsDiv.style.display = "block";
+                if (studyToolDiv) {
+                    studyToolDiv.style.display = "none";
+                }
+            }
+        };
     }
-}
+
+    function openRoute(card) {
+        const route = card.getAttribute("data-route");
+        if (!route) return;
+        window.location.href = route;
+    }
+
+    cards.forEach((card) => {
+        card.addEventListener("click", function () {
+            openRoute(card);
+        });
+
+        card.addEventListener("keydown", function (event) {
+            if (event.key === "Enter" || event.key === " ") {
+                event.preventDefault();
+                openRoute(card);
+            }
+        });
+    });
+})();
